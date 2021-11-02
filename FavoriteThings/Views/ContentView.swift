0e3b2c1a1960b6c880017c95c579f9e1637a0320
Lillian Yang <lyang@lcs.on.ca>
2021-11-02 14:33:45 -0400

    Added a photo caption and credit to content view.

--- FavoriteThings/Views/ContentView.swift
+++ FavoriteThings/Views/ContentView.swift
@@ -12,15 +12,33 @@ struct ContentView: View {
         
         ScrollView{
             
-            VStack{
+            VStack(alignment: .leading){
                 
-                Image ("Kirby")
-                    .resizable()
+                VStack (alignment:.leading, spacing: 10){
+                    Image ("Kirby")
+                        .resizable()
                     .scaledToFit()
+                    
+                    VStack(alignment: .leading){
+                        Text("A really blurry picture or Kirby singing.")
+                            .font(.caption)
+                            .bold()
+                        
+                        Text("Photo credit: kirbynation")
+                            .font(.caption)
+                            .italic()
+                        
+                    }
+                    .padding(.horizontal)
+                }
                 
                 
-                Text("Kirby singing")
-                
+                Text("""
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempus commodo ex vel efficitur. Cras congue congue laoreet. Aliquam vehicula sollicitudin lectus a imperdiet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris ultricies magna at bibendum vehicula. Proin tortor velit, pellentesque ut tincidunt nec, malesuada id elit. Suspendisse aliquam rhoncus purus at imperdiet. Curabitur finibus risus id dui scelerisque congue.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tempus commodo ex vel efficitur. Cras congue congue laoreet. Aliquam vehicula sollicitudin lectus a imperdiet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris ultricies magna at bibendum vehicula. Proin tortor velit, pellentesque ut tincidunt nec, malesuada id elit. Suspendisse aliquam rhoncus purus at imperdiet. Curabitur finibus risus id dui scelerisque congue.
+""")
+                    .padding()
                 Spacer()
      
             }
